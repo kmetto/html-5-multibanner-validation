72e875ec78a5fe4e8ae8d67675978bb748ba71f4
Konstantin <met9127@gmail.com>
2016-10-22 13:16:07 +0300

    report

--- core.php
+++ core.php
@@ -2,6 +2,7 @@
 
 require_once "vendor/autoload.php";
 
+use Symfony\Component\DomCrawler\Crawler;
 ob_implicit_flush();
 
 $guzzle = new \GuzzleHttp\Client();
@@ -27,16 +28,27 @@ function readDirectory($dirName){
     return $dirCont;
 }
 
+function getResult($url){
+  $html = file_get_contents($url);
+  $crawler = new Crawler($html);
+  $results = [];
+  $crawler->filter("md-list-item")->each(function(Crawler $node, $i) use (&$results){
+    if($node->attr('ga-category')=="Results"){
+      $results[$node->attr('ga-label')] = preg_match("/.*pass.*/",$node->attr('class'))?true:false;
+    }
+  });
 
+  return $results;
+}
 
-echo '<details><summary></summary><p><pre>';
-print_r($tree = readDirectory($dirname));
-echo '</pre></p></details>';
-
+// echo '<details><summary></summary><p><pre>';
+// print_r($tree = readDirectory($dirname));
+// echo '</pre></p></details>';
+//
 $tree = readDirectory($dirname);
 
 array_walk_recursive($tree, function($val, $key) use ($guzzle){
-    echo "<div class='line'><span class='date'>".date("G:i:s",microtime(true))."</span><span class='message'>Начало проверки файла".$val."</span></div>";
+    echo "<hr><div class='line'><span class='date'>".date("G:i:s",microtime(true))."</span><span class='message'> Начало проверки файла ".$val."</span></div>";
     $response =  $guzzle->request("POST", "https://h5validator.appspot.com/api/policy/adwords", [
         'multipart' => [
             [
@@ -53,9 +65,15 @@ array_walk_recursive($tree, function($val, $key) use ($guzzle){
     $response = $response->getBody()->getContents();
     $response = json_decode(substr($response, strpos($response,',')+1), false, 512, JSON_BIGINT_AS_STRING);
 
-    echo date("G:i:s",microtime(true))." Файл ".basename($val)." обработан, <a href=\"https://h5validator.appspot.com/adwords/result/{$response->response->result}\" target='_blank'>отчет</a><br>-------------------<br>";
+    echo date("G:i:s",microtime(true))." Файл ".basename($val)." обработан, <a href=\"https://h5validator.appspot.com/adwords/result/{$response->response->result}\" target='_blank'>отчет</a><br><br>";
+    $result = getResult("https://h5validator.appspot.com/adwords/result/{$response->response->result}");
+    if(!empty($result)){
+      foreach ($result as $check => $value) {
+        $status = ($value)? "<span style='color:green'>OK</span>":"<span style='color:red'>ERROR</span>";
+        echo $check." - ".$status."<br>";
+      }
 
+    }
 });
 
 echo "<br><br>"."Все баннеры проверены";
-
