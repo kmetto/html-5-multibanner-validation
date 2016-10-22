7a1001406d9f95cb784b0b4ab18d84578b0fc74b
Konstantin <met9127@gmail.com>
2016-10-22 14:48:14 +0300

    catch exception

--- core.php
+++ core.php
@@ -9,8 +9,6 @@ $guzzle = new \GuzzleHttp\Client();
 
 $dirname = __DIR__."/files";
 
-
-
 function readDirectory($dirName){
     $dir = dir($dirName);
     $dirCont = [];
@@ -41,38 +39,39 @@ function getResult($url){
   return $results;
 }
 
-// echo '<details><summary></summary><p><pre>';
-// print_r($tree = readDirectory($dirname));
-// echo '</pre></p></details>';
-//
 $tree = readDirectory($dirname);
 
 array_walk_recursive($tree, function($val, $key) use ($guzzle){
+
     echo "<hr><div class='line'><span class='date'>".date("G:i:s",microtime(true))."</span><span class='message'> Начало проверки файла ".$val."</span></div>";
-    $response =  $guzzle->request("POST", "https://h5validator.appspot.com/api/policy/adwords", [
-        'multipart' => [
-            [
-                'name'     => "creative_bundle",
-                'contents' => fopen($val, 'r'),
-                'headers'  => [
-                    'Content-Type' => 'application/zip',
-                    'Content-Disposition' => "form-data; name=creative_bundle; filename=".basename($val).";"
-                ]
-            ]
-        ]
-    ]);
+    try {
+      $response =  $guzzle->request("POST", "https://h5validator.appspot.com/api/policy/adwords", [
+          'multipart' => [
+              [
+                  'name'     => "creative_bundle",
+                  'contents' => fopen($val, 'r'),
+                  'headers'  => [
+                      'Content-Type' => 'application/zip',
+                      'Content-Disposition' => "form-data; name=creative_bundle; filename=".basename($val).";"
+                  ]
+              ]
+          ]
+      ]);
 
-    $response = $response->getBody()->getContents();
-    $response = json_decode(substr($response, strpos($response,',')+1), false, 512, JSON_BIGINT_AS_STRING);
+      $response = $response->getBody()->getContents();
+      $response = json_decode(substr($response, strpos($response,',')+1), false, 512, JSON_BIGINT_AS_STRING);
 
-    echo date("G:i:s",microtime(true))." Файл ".basename($val)." обработан, <a href=\"https://h5validator.appspot.com/adwords/result/{$response->response->result}\" target='_blank'>отчет</a><br><br>";
-    $result = getResult("https://h5validator.appspot.com/adwords/result/{$response->response->result}");
-    if(!empty($result)){
-      foreach ($result as $check => $value) {
-        $status = ($value)? "<span style='color:green'>OK</span>":"<span style='color:red'>ERROR</span>";
-        echo $check." - ".$status."<br>";
+      echo date("G:i:s",microtime(true))." Файл ".basename($val)." обработан, <a href=\"https://h5validator.appspot.com/adwords/result/{$response->response->result}\" target='_blank'>отчет</a><br><br>";
+      $result = getResult("https://h5validator.appspot.com/adwords/result/{$response->response->result}");
+      if(!empty($result)){
+        foreach ($result as $check => $value) {
+          $status = ($value)? "<span style='color:green'>OK</span>":"<span style='color:red'>ERROR</span>";
+          echo $check." - ".$status."<br>";
+        }
+
       }
-
+    } catch (Exception $e) {
+      echo $e->getMessage();
     }
 });
 
